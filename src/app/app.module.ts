dc90ed2de75d0a6bee85832b949bcc118c579d14
mabbaszadegan <mhmabbaszadegan@gmail.com>
2018-10-10 00:52:34 +0330

    تغییرات ظاهری جهت نمایش لیست

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
-
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './shared/err404/err404.component'
 
